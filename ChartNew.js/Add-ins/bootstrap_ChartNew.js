1c2550439bc2a9a52820278c4a540cc1d2072fb5
Francois Vancoppenolle <francois.vancoppenolle@favomo.be>
2016-01-03 13:43:31 +0100

    Update bootstrap_ChartNew.js

--- ChartNew.js/Add-ins/bootstrap_ChartNew.js
+++ ChartNew.js/Add-ins/bootstrap_ChartNew.js
@@ -22,11 +22,14 @@ function setCanvas(tpgraph,data,options,height,width) {
 	if(typeof tag_canvas_list[divs[divs.length-1].id]=="undefined") tag_canvas_list[divs[divs.length-1].id] = [];
 	var cnvcnt=tag_canvas_list[divs[divs.length-1].id].length;
 	document.write("<canvas id=\""+divs[divs.length-1].id+tag_canvas_suffix+"_"+cnvcnt+"\" height=\""+setheight+"\" width=\""+setwidth+"\"></canvas>");
-	tag_canvas_list[divs[divs.length-1].id][tag_canvas_list[divs[divs.length-1].id].length]={ tpgraph:tpgraph,data:data,options:options, firstDisplay:true };
+	tag_canvas_list[divs[divs.length-1].id][tag_canvas_list[divs[divs.length-1].id].length]={ firstDisplay: true, width:setwidth, height: setheight, tpgraph:tpgraph,data:data,options:options, runAnimation:true };
+        document.getElementById(divs[divs.length-1].id+tag_canvas_suffix+"_"+cnvcnt).getContext("2d").original_height=setheight;
+        document.getElementById(divs[divs.length-1].id+tag_canvas_suffix+"_"+cnvcnt).getContext("2d").original_width=setwidth;
 	if(divs[divs.length-1].id==first_tag){
 		console.log("Display Canvas: "+divs[divs.length-1].id+tag_canvas_suffix+"_"+cnvcnt);
-		tab_disp_canvas(divs[divs.length-1].id+tag_canvas_suffix+"_"+cnvcnt,tpgraph,data,options,true);
-		if(tag_animation_only_at_first_display==true)tag_canvas_list[divs[divs.length-1].id][tag_canvas_list[divs[divs.length-1].id].length-1]["firstDisplay"]=false;
+		tab_disp_canvas(divs[divs.length-1].id+tag_canvas_suffix+"_"+cnvcnt,tpgraph,data,options,true,setwidth,setheight,true);
+		if(tag_animation_only_at_first_display==true)tag_canvas_list[divs[divs.length-1].id][tag_canvas_list[divs[divs.length-1].id].length-1]["runAnimation"]=false;
+		tag_canvas_list[divs[divs.length-1].id][tag_canvas_list[divs[divs.length-1].id].length-1]["firstDisplay"]=false;	
 	}
 };
 
@@ -34,14 +37,41 @@ function setCanvas(tpgraph,data,options,height,width) {
 function tab_disp_graph(tab){
 	if(typeof tag_canvas_list[tab]!="undefined") {
 		for(var i=0;i<tag_canvas_list[tab].length;i++) {
-			tab_disp_canvas(tab+tag_canvas_suffix+"_"+i,tag_canvas_list[tab][i]["tpgraph"],tag_canvas_list[tab][i]["data"],tag_canvas_list[tab][i]["options"],tag_canvas_list[tab][i]["firstDisplay"]);
-			if(tag_animation_only_at_first_display==true)tag_canvas_list[tab][i]["firstDisplay"]=false;			
+			tab_disp_canvas(tab+tag_canvas_suffix+"_"+i,tag_canvas_list[tab][i]["tpgraph"],tag_canvas_list[tab][i]["data"],tag_canvas_list[tab][i]["options"],tag_canvas_list[tab][i]["runAnimation"],tag_canvas_list[tab][i]["width"],tag_canvas_list[tab][i]["height"],tag_canvas_list[tab][i]["firstDisplay"]);
+			if(tag_animation_only_at_first_display==true)tag_canvas_list[tab][i]["runAnimation"]=false;
+			tag_canvas_list[tab][i]["firstDisplay"]=false;			
 		}
 	}
 };
 
-function tab_disp_canvas(canvas,tpgraph,data,options,firstDisplay){
-	if(!firstDisplay)return(false);
+function tab_disp_canvas(canvas,tpgraph,data,options,runAnimation,setWidth,setHeight,firstDisp){
+
+	if(!runAnimation)return(false);
+
+	if(!firstDisp) {
+//	if(firstDisp==2) {
+console.log("Draw Not First Time" + document.getElementById(canvas).getContext("2d").firstPass);
+if(document.getElementById(canvas).getContext("2d").firstPass==3)document.getElementById(canvas).getContext("2d").firstPass=5;
+//console.log("Size:"+document.getElementById(canvas).getContext("2d").original_height+" "+document.getElementById(canvas).getContext("2d").original_width);
+updateChart(document.getElementById(canvas).getContext("2d"),data,options,true,true);
+//    	document.getElementById(canvas).getContext("2d").canvas.height=setWidth;
+//    	document.getElementById(canvas).getContext("2d").canvas.width=setHeight;
+    
+//    	document.getElementById(canvas).getContext("2d").firstPass=undefined;
+//    	document.getElementById(canvas).getContext("2d").runanimationcompletefunction=true;
+//    	document.getElementById(canvas).getContext("2d").tpchart=undefined;
+//    	document.getElementById(canvas).getContext("2d").initialWidth=undefined;
+//    	document.getElementById(canvas).getContext("2d").chartTextScale=undefined;
+//    	document.getElementById(canvas).getContext("2d").chartLineScale=undefined;
+//    	document.getElementById(canvas).getContext("2d").chartSpaceScale=undefined;
+//    	document.getElementById(canvas).getContext("2d").ChartNewId=undefined;
+//    	document.getElementById(canvas).getContext("2d").DefaultchartTextScale=undefined;
+//    	document.getElementById(canvas).getContext("2d").DefaultchartLineScale=undefined;
+//    	document.getElementById(canvas).getContext("2d").DefaultchartSpaceScale=undefined;
+//    	document.getElementById(canvas).getContext("2d").aspectRatio = undefined;    
+}
+else {
+console.log("DRAW First Time");
 	switch (tpgraph) {
 		case "Bar":
 			var chart = new Chart(document.getElementById(canvas).getContext("2d")).Bar(data,options);
@@ -72,6 +102,7 @@ function tab_disp_canvas(canvas,tpgraph,data,options,firstDisplay){
 			var chart = new Chart(document.getElementById(canvas).getContext("2d")).Line(data,options);
 			break;
 	}
+}
 };
 
 function setRefreshCanvas() {
@@ -79,7 +110,9 @@ $('a[data-toggle=tab]').on('shown.bs.tab', function (e) {
 var vl_target=e.target+"";
 vl_target=vl_target.split("#").pop();	
 current_tag=vl_target;
+console.log("Before resize");
 chartJsResize();
+console.log("After resize");
 tab_disp_graph(vl_target);
 });
 };
