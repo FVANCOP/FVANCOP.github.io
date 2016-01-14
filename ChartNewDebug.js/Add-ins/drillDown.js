68a2e226b601bc2040e4d0438041b42e42d49bfa
Francois Vancoppenolle <francois.vancoppenolle@favomo.be>
2016-01-14 22:31:38 +0100

    Update drillDown.js

--- ChartNewDebug.js/Add-ins/drillDown.js
+++ ChartNewDebug.js/Add-ins/drillDown.js
@@ -7,34 +7,34 @@ var posDrillDownBackData=0;
 
 function chartDrillUp(event,ctx,config,data,other)
 {
-	window.alert("IN DRILL UP");
 	if(posDrillDownBackData>0) {
 		posDrillDownBackData--;
-		updateChart(ctx,drillDownBackData[posDrillDownBackData],drillDownBackConfig[posDrillDownBackData],false,false);
+          	ctx.fistPass=undefined;
+		eval("var newchart=new Chart(document.getElementById(\"canvas_bar\").getContext(\"2d\"))."+ctx.tpchart+"(drillDownBackData[posDrillDownBackData],drillDownBackConfig[posDrillDownBackData]);");
+//		updateChart(ctx,drillDownBackData[posDrillDownBackData],drillDownBackConfig[posDrillDownBackData],false,false);
 	}
 }
 
 function chartDrillDown(event,ctx,config,data,other)
 {
-  window.alert("IN DRILL DOWN")
   if(other != null) {
-  	window.alert("NOT UP "+other.i);
   	if(data.datasets[other.i].drillDownData != null) {
   		if(data.datasets[other.i].drillDownData[other.j] != null) {
   			var vconfig=config;
 	  		if(data.datasets[other.i].drillDownConfig != null)if(data.datasets[other.i].drillDownConfig[other.j] != null)vconfig=data.datasets[other.i].drillDownConfig[other.j];
 	          	drillDownBackConfig[posDrillDownBackData]=config;
 	          	drillDownBackData[posDrillDownBackData++]=data;
-			updateChart(ctx,data.datasets[other.i].drillDownData[other.j],vconfig,false,false);
+	          	ctx.fistPass=undefined;
+			eval("var newchart=new Chart(document.getElementById(\"canvas_bar\").getContext(\"2d\"))."+ctx.tpchart+"(data.datasets[other.i].drillDownData[other.j],vconfig);");
+//			updateChart(ctx,data.datasets[other.i].drillDownData[other.j],vconfig,false,false);
 		}
 	}
   } else if(config.drillOutsideUp){
-  	window.alert("UP");
 	if(posDrillDownBackData>0) {
 		posDrillDownBackData--;
-		updateChart(ctx,drillDownBackData[posDrillDownBackData],drillDownBackConfig[posDrillDownBackData],false,false);
+          	ctx.fistPass=undefined;
+		eval("var newchart=new Chart(document.getElementById(\"canvas_bar\").getContext(\"2d\"))."+ctx.tpchart+"(drillDownBackData[posDrillDownBackData],drillDownBackConfig[posDrillDownBackData]);");
+//		updateChart(ctx,drillDownBackData[posDrillDownBackData],drillDownBackConfig[posDrillDownBackData],false,false);
 	}
-  } else {
-  	window.alert("NONE");
   }
 }
