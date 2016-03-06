ee74b105574a78839081f641a5790bc6269f079f
Francois Vancoppenolle <francois.vancoppenolle@favomo.be>
2016-03-06 14:44:45 +0100

    Update ChartNew.js

--- ChartNewDebug.js/ChartNew.js
+++ ChartNewDebug.js/ChartNew.js
@@ -1324,8 +1324,8 @@ window.Chart = function(context) {
 	var height = context.canvas.height;
 	//High pixel density displays - multiply the size of the canvas height/width by the device pixel ratio, then scale.
 	if (window.devicePixelRatio) {
-		context.canvas.style.width = width + "px";
-		context.canvas.style.height = height + "px";
+		// context.canvas.style.width = width + "px";
+		// context.canvas.style.height = height + "px";
 		context.canvas.height = height * window.devicePixelRatio;
 		context.canvas.width = width * window.devicePixelRatio;
 		context.scale(window.devicePixelRatio, window.devicePixelRatio);
