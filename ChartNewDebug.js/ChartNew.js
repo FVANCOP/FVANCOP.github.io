77770e3f9a9579466c056bac692854565b590259
Francois Vancoppenolle <francois.vancoppenolle@favomo.be>
2017-01-22 09:21:46 +0100

    Update ChartNew.js

--- ChartNewDebug.js/ChartNew.js
+++ ChartNewDebug.js/ChartNew.js
@@ -1,3 +1,5 @@
+console.log("INIT");
+
 /*              
  * ChartNew.js  
  *                                                                                                                                                                           
